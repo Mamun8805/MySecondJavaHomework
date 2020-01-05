7ac6fe0697eb14df2a5a07f4969002b3f0e8d76d
Mamun Miah <Mamun8805@outlook.com>
2020-01-05 10:16:27 -0500

    adding new file

---
+++ src/com/ga/secondjavahw/Sundayhomework.java
@@ -0,0 +1,62 @@
+package com.ga.secondjavahw;
+
+public class Sundayhomework {
+
+    public static void main(String [] args){
+
+    System.out.println( "Sundayhomework" );
+    myFirstNbaTeam();
+    mySecondNbaTeam();
+    myThirdNbaTeam();
+    myFourthNbaTeam();
+    myFifthNbaTeam();
+
+
+}
+    public static void myFirstNbaTeam(){
+        //my first method
+        int ten=10;
+        int twenty=20;
+        System.out.println(ten+twenty);
+    }
+    //my second method
+    public static void mySecondNbaTeam(){
+
+        int second=200;
+        String third="Pistons";
+        System.out.println(200+"Pistons");
+}
+    //my third method
+    public static void myThirdNbaTeam(){
+
+    int fifth=616;
+    int fourth=84;
+    System.out.println(fifth+fourth);
+}
+    //my fourth method
+    public static void myFourthNbaTeam(){
+    String sixth="Pistons 2020 NBA champs";
+    System.out.println(sixth);
+    }
+
+    //my fifth method
+    //concatanation
+    public static void myFifthNbaTeam(){
+     int totalPointsInRegularSeason=30000;
+     int totalPointsInPlayoffs=12000;
+     String stephCurry="total career points";
+     System.out.println(totalPointsInRegularSeason+totalPointsInPlayoffs);
+     System.out.println(stephCurry);
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
